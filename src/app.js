84db4b6f886cc8e933c3f8769bb4382486f155dd
JessFun97 <138305875+JessFun97@users.noreply.github.com>
2023-08-23 21:07:37 +0100

    Added API call for the forecast in the  JS file

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.list);
   let forecastElement = document.querySelector("#forecast-day");
 
   let days = ["Wed", "Thur", "Fri", "Sat", "Sun", "Mon"];
@@ -51,6 +51,14 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function acquireForecast(coordinates) {
+  console.log(coordinates);
+
+  let apiKey = "2e45eaecbf0d17607244af8d840a698f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -70,6 +78,8 @@ function displayTemperature(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  acquireForecast(response.data.coord);
 }
 
 function search(city) {
@@ -86,8 +96,6 @@ function handlePush(event) {
 }
 
 search("Lagos");
-
-displayForecast();
 
 let form = document.querySelector("#searching");
 form.addEventListener("submit", handlePush);
