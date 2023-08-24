797f2d2b9da6557a47d09de12d74d48452656e64
JessFun97 <138305875+JessFun97@users.noreply.github.com>
2023-08-24 22:18:08 +0100

    Removed the Fahrenheit conversion link

--- src/app.js
+++ src/app.js
@@ -30,7 +30,6 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data.daily);
   let prediction = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast-day");
@@ -70,8 +69,6 @@ function displayForecast(response) {
 }
 
 function acquireForecast(coordinates) {
-  console.log(coordinates);
-
   let apiKey = "c03face7caa58a9b7ffa9f52b7238a93";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
